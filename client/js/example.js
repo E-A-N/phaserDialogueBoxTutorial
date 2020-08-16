7fe98214a6b836627e45240993211c8ebd6f2787
e-a-n <ean.director@gmail.com>
2020-08-15 19:58:33 -0700

    currently trying to debug images not appearing in dialogue box

--- client/js/example.js
+++ client/js/example.js
@@ -3,9 +3,29 @@ let exampleState = {
     create: () => {
         game.stage.setBackgroundColor('#777777');
         console.log("example state has been activated");
+        let boxSettings = config.default.dialogue.settings;
+
+
+        let antData =  [config.menuState.logo.x, config.menuState.logo.y, config.menuState.logo.spriteKey];
+
+        let diaImageData ={
+            images: antData,
+            hasNewImages: true,
+            clearCurrentImages: false
+        } 
+
+
 
         let dia = PhaserDialogue();
-        dia.init(game, config.default.dialogue.settings);
-        dia.displayMessage("Hello World!", null, true);
+        dia.init(game, boxSettings);
+        dia.displayMessage("Hello World!", diaImageData, true);
+        //    .displayMessage("This is the next one bb!")
+        //    .displayMessage("Type writing the next message just to make sure!", null, true)
+
+        // boxSettings.typeDelay = 0.2
+        // let d2 = PhaserDialogue();
+        // d2.init(game, boxSettings);
+        // d2.container.y = 0;
+        // d2.displayMessage("This should be from a bit higher", null, true);
     },
 };
