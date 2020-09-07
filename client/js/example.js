3619ed52a99a855488ec65f530f4b72c4ff008c3
e-a-n <ean.director@gmail.com>
2020-09-06 23:46:45 -0700

    tested new image feature in the phaserDialogueBox dependency and the clearCurrentImages paramater key.  preparing to do a more thorough test with images speaking dialouge!

--- client/js/example.js
+++ client/js/example.js
@@ -6,26 +6,27 @@ let exampleState = {
         let boxSettings = config.default.dialogue.settings;
 
 
-        let antData =  [config.menuState.logo.x, config.menuState.logo.y, config.menuState.logo.spriteKey];
+        //let antData =  [config.menuState.logo.x, config.menuState.logo.y, config.menuState.logo.spriteKey];
 
-        let diaImageData ={
-            images: antData,
+        let antData =  [0, 0, config.menuState.logo.spriteKey];
+        let diaImageData = {
+            images: [antData],
             hasNewImages: true,
             clearCurrentImages: false
         } 
 
 
-
         let dia = PhaserDialogue();
         dia.init(game, boxSettings);
-        dia.displayMessage("Hello World!", diaImageData, true);
-        //    .displayMessage("This is the next one bb!")
-        //    .displayMessage("Type writing the next message just to make sure!", null, true)
+        dia.displayMessage("Hello World!", diaImageData, true)
+           .displayMessage("This is the next one bb!", {clearCurrentImages: true})
+           .displayMessage("Type writing the next message just to make sure!", null, true)
 
         // boxSettings.typeDelay = 0.2
         // let d2 = PhaserDialogue();
         // d2.init(game, boxSettings);
         // d2.container.y = 0;
         // d2.displayMessage("This should be from a bit higher", null, true);
-    },
+
+    }
 };
